9014571b408f22f9bce5e0f139d5584cc37f350e
srinivash <unitofsrinivash@gmail.com>
2026-06-12 08:47:33 +0530

    Refactor getChannelStats to improve readability and structure; add aggregation for total views, videos, likes, and subscribers

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,99 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { count } from "console";
+import { channel } from "diagnostics_channel";
+import { lookup } from "dns";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const viewsStats = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalViews: {
+          $sum: "$views",
+        },
+      },
+    },
+  ]);
+
+  const totalViews = viewsStats[0]?.totalViews || 0;
+
+  const totalVideos = await Video.countDocuments({
+    owner: req.user._id,
+  });
+
+  const totalLikes = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalLikes: {
+          $sum: "$likesCount",
+        },
+      },
+    },
+  ]);
+
+  //Use aggregate for multiple stages this ao works but mehh
+
+  // const subscribersStats = await Subscription.aggregate([
+  //   {
+  //     $match: {
+  //       channel: new mongoose.Types.ObjectId(req.user._id),
+  //     },
+  //   },
+  //   {
+  //     $count: "totalSubscribers",
+  //   },
+  // ]);
+
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: req.user._id,
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { totalViews, totalVideos, totalSubscribers, totalLikes },
+        "Channel stats fetched successfully"
+      )
+    );
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  // TODO: Get all the videos uploaded by the channel
+});
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+export { getChannelStats, getChannelVideos };
